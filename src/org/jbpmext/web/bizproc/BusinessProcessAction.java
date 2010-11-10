35b2ccb138619d47c175f43baf146196bdd412c7
weihthchk <weihthchk@dff8774a-143c-b3a6-6ee3-aef0fb1ac8f5>
2010-11-10 09:31:29 +0000

    Process designer added.

--- src/org/jbpmext/web/bizproc/BusinessProcessAction.java
+++ src/org/jbpmext/web/bizproc/BusinessProcessAction.java
@@ -3,11 +3,16 @@
  */
 package org.jbpmext.web.bizproc;
 
+import java.io.File;
+import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.List;
 
+import net.sf.json.JSONObject;
+
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
+import org.apache.struts2.ServletActionContext;
 import org.apache.struts2.convention.annotation.Action;
 import org.apache.struts2.convention.annotation.Namespace;
 import org.apache.struts2.convention.annotation.Result;
@@ -18,6 +23,9 @@ import org.jbpmext.util.EasyUIGridWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.opensymphony.xwork2.ActionSupport;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
 
 /**
  * @author weiht
@@ -32,6 +40,7 @@ public class BusinessProcessAction extends ActionSupport {
 	
 	private List<ProcessDefinition> definitions;
 	private ProcessDefinition definition;
+	private String data;
 
 	@Override
 	@Action(value="manager", results={@Result(name="success", location="/WEB-INF/content/bizproc/manager.jsp")})
@@ -47,12 +56,40 @@ public class BusinessProcessAction extends ActionSupport {
 			logger.error("Loading process definitions:", ex);
 			definitions = new ArrayList<ProcessDefinition>();
 		}
-		ActionJsonUtil.putJson(new EasyUIGridWrapper(definitions));
+		ActionJsonUtil.putJson(new EasyUIGridWrapper(procDefsToStrings(definitions)));
 		return SUCCESS;
 	}
 	
+	private List<String> procDefsToStrings(List<ProcessDefinition> defs) {
+		List<String> result = new ArrayList<String>(defs.size());
+		for (ProcessDefinition def: defs) {
+			//def.
+		}
+		return result;
+	}
+
 	@Action(value="save", results={@Result(name="success", location="/common/json.jsp")})
 	public String save() throws Exception {
+		logger.debug(data);
+		JSONObject dataMap = JSONObject.fromObject(data);
+		dataMap.put("myflowdata", "<![CDATA[" + data + "]]>");
+
+		Configuration config = new Configuration();
+		try {
+
+			config.setDirectoryForTemplateLoading(new File(ServletActionContext
+					.getServletContext().getRealPath("/WEB-INF/classes/freemarker/")));
+			Template temp = config.getTemplate("jpdl-template.tpl", "utf-8");
+			StringWriter w = new StringWriter();
+			temp.process(dataMap, w);
+			logger.debug(w);
+			this.repositoryService.createDeployment().addResourceFromString(
+					"test.jpdl.xml", w.toString()).deploy();
+			ActionJsonUtil.putJson(data);
+		} catch (Exception ex) {
+			logger.error("Saving process definition:", ex);
+			ActionJsonUtil.putJson(new Object());
+		}
 		return SUCCESS;
 	}
 	
@@ -67,5 +104,13 @@ public class BusinessProcessAction extends ActionSupport {
 
 	public void setDefinition(ProcessDefinition definition) {
 		this.definition = definition;
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
 	}
 }
